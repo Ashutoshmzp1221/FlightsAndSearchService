6625fca86a0a3923631913ed3f4ca6d4d0650a64
Ashutoshmzp1221 <ashutoshmzp1221@gmail.com>
2024-08-08 23:16:18 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,24 +3,53 @@ const { City } = require('../models/index');
 class CityRepository {
     async createCity({ name }){
         try {
-            const city = await City.create({ name })
+            const city = await City.create({ 
+                name 
+            });
             return city;
         } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
 
-    async deleteCity({ cityId }){
+    async deleteCity(cityId){
         try {
             await City.destroy({
                 where : {
                     id : cityId,
                 }
             });
+            return true;
         } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw(error);
         }
     }
-}
+
+    async updateCity(cityId, data) {
+        try {
+            const city = City.update(data, {
+                where: {
+                    id: cityId
+                }
+            })
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw(error);
+        }
+    }
+
+    async getCitry(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw(error);
+        }
+    }
+ }
 
 module.exports = CityRepository;
