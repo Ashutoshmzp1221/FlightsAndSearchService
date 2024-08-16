3dc90c7d460f500968f11bc455a12780771b53eb
Ashutoshmzp1221 <ashutoshmzp1221@gmail.com>
2024-08-16 09:30:49 +0530

    Added airplanes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const { PORT } = require('./config/serverConfig');
-const ApiRoutes = require('./routes/index')
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = 3000
+// const { PORT } = require('./config/serverConfig');
+const ApiRoutes = require('./routes/index');
 
 const db = require('./models/index');
-const setupAndStartServer =  () => {
+// const {Airplane} = require('./models/index');
+
+const setupAndStartServer = async () => {
 
     // create the express object
     const app = express();
 
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.urlencoded({extended: true}));
 
     app.use('/api', ApiRoutes);
-    
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server started at ${PORT}`);
-        if(process.env.SYNC_DB){
+        if(process.env.SYNC_DB) {
             db.sequelize.sync({alter: true});
         }
     });
 }
 
-setupAndStartServer();  
+setupAndStartServer();
