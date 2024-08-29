5c752b54f295efffa8d7046f08586d4abe1b2ddc
Ashutoshmzp1221 <ashutoshmzp1221@gmail.com>
2024-08-29 10:05:35 +0530

    Added error code enum

--- src/middlewares/flight-middlewares.js
+++ src/middlewares/flight-middlewares.js
@@ -1,14 +1,15 @@
+const { ClientErrorsCodes } = require('../utils/error-codes')
 const validateCreateFlight = (req, res, next) => {
     if(
         !req.body.flightNumber ||
         !req.body.airplaneId   ||
         !req.body.departureAirportId ||
         !req.body.arrivalAirportId   ||
-        !req.body.arrivalTime    ||
-        !req.body.departureTime  ||
+        !req.body.arrivalTime   ||
+        !req.body.departureTime ||
         !req.body.price
     ) {
-        return res.status(400).json({
+        return res.status(ClientErrorsCodes.BAD_REQUEST).json({
             data: {},
             success: false,
             message: 'Invalid request body for create flight',
